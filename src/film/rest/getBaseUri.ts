9cf52a96f6896aa9a72b2f56586c004ba5362c48
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-04-13 10:54:48 +0200

    Add: Helper that determines base uri from request

---
+++ src/film/rest/getBaseUri.ts
@@ -0,0 +1,22 @@
+import { FilmReadService } from '../service/film-read.service.js';
+import { type Request } from 'express';
+import { cloud } from '../../config/cloud.js';
+import { nodeConfig } from '../../config/node.js';
+
+const port = cloud === undefined ? `:${nodeConfig.port}` : '';
+
+export const getBaseUri = (req: Request) => {
+    const { protocol, hostname, url } = req;
+
+    let basePath = url.includes('?') ? url.slice(0, url.lastIndexOf('?')) : url;
+
+    const indexLastSlash = basePath.lastIndexOf('/');
+    if (indexLastSlash > 0) {
+        const idStr = basePath.slice(indexLastSlash + 1);
+        if (FilmReadService.ID_PATTERN.test(idStr)) {
+            basePath = basePath.slice(0, indexLastSlash);
+        }
+    }
+
+    return `${protocol}://${hostname}/${port}/${basePath}`;
+};
