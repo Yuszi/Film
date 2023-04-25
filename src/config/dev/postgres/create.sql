5feb1eb4129fbe97c12994f73b3e077b820a6684
Luca Burghardt <bulu1021@h-ka.de>
2023-04-25 18:04:26 +0200

    SQL Anpassung

--- src/config/dev/postgres/create.sql
+++ src/config/dev/postgres/create.sql
@@ -1,76 +1,29 @@
--- Copyright (C) 2022 - present Juergen Zimmermann, Hochschule Karlsruhe
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
--- docker compose exec postgres bash
--- psql --dbname=film --username=film --file=/scripts/create-table-film.sql
-
--- https://www.postgresql.org/docs/devel/app-psql.html
--- https://www.postgresql.org/docs/current/ddl-schemas.html
--- https://www.postgresql.org/docs/current/ddl-schemas.html#DDL-SCHEMAS-CREATE
--- "user-private schema" (Default-Schema: public)
 CREATE SCHEMA IF NOT EXISTS AUTHORIZATION film;
 
 ALTER ROLE film SET search_path = 'film';
 
--- https://www.postgresql.org/docs/current/sql-createtype.html
--- https://www.postgresql.org/docs/current/datatype-enum.html
 CREATE TYPE filmart AS ENUM ('DRUCKAUSGABE', 'KINDLE');
 
--- https://www.postgresql.org/docs/current/sql-createtable.html
--- https://www.postgresql.org/docs/current/datatype.html
 CREATE TABLE IF NOT EXISTS film (
-                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-PRIMARY-KEYS
-                  -- impliziter Index fuer Primary Key
-    id            integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#id-1.5.4.6.6
-    version       integer NOT NULL DEFAULT 0,
-                  -- impliziter Index als B-Baum durch UNIQUE
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    name          varchar(17) NOT NULL UNIQUE USING INDEX TABLESPACE filmspace,
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
-                  -- https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
-    rating        integer NOT NULL CHECK (rating >= 0 AND rating <= 5),
-    art           filmart,
-                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
-                  -- 10 Stellen, davon 2 Nachkommastellen
-    preis         decimal(8,2) NOT NULL,
-    rabatt        decimal(4,3) NOT NULL,
-                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
-    lieferbar     boolean NOT NULL DEFAULT FALSE,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    datum         date,
-    homepage      varchar(40),
-    schlagwoerter varchar(64),
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    erzeugt       timestamp NOT NULL DEFAULT NOW(),
-    aktualisiert  timestamp NOT NULL DEFAULT NOW()
+    id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
+    version             integer NOT NULL DEFAULT 0,
+    name                varchar(17) NOT NULL UNIQUE USING INDEX TABLESPACE filmspace,
+    sprache             varchar(40),
+    genre               filmart,
+    rating              integer NOT NULL DEFAULT 0,
+    lieferbar           boolean NOT NULL DEFAULT FALSE,
+    erscheinungsjahr    date,
+    schlagwoerter       varchar(64),
+    hauptdarsteller     hauptdarsteller,
+    erzeugt             timestamp NOT NULL DEFAULT NOW(),
+    aktualisiert        timestamp NOT NULL DEFAULT NOW()
 ) TABLESPACE filmspace;
 
 CREATE TABLE IF NOT EXISTS hauptdarsteller (
     id          integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
-    hauptdarsteller       varchar(40) NOT NULL,
-    nebendarsteller  varchar(40),
-    film_id     integer NOT NULL UNIQUE USING INDEX TABLESPACE filmspace REFERENCES film
+    rolle       varchar(40) NOT NULL,
+    vorname     varchar(40),
+    nachname    varchar(40),
+    alter       integer NOT NULL DEFAULT 0,
+    film        film
 ) TABLESPACE filmspace;
-
-
-CREATE TABLE IF NOT EXISTS abbildung (
-    id              integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
-    beschriftung    varchar(32) NOT NULL,
-    content_type    varchar(16) NOT NULL,
-    film_id         integer NOT NULL REFERENCES film
-) TABLESPACE filmspace;
-CREATE INDEX IF NOT EXISTS abbildung_film_id_idx ON abbildung(film_id) TABLESPACE filmspace;
