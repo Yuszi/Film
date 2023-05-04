8ca73dc12bff3762772bf09e95dd67b6501814a0
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-05-04 12:53:56 +0200

    Fix: Fix SQL

--- src/config/dev/postgres/create.sql
+++ src/config/dev/postgres/create.sql
@@ -3,16 +3,15 @@ CREATE SCHEMA IF NOT EXISTS AUTHORIZATION film;
 ALTER ROLE film SET search_path = 'film';
 
 DROP TYPE IF EXISTS filmart;
-CREATE TYPE filmart AS ENUM ('DRUCKAUSGABE', 'KINDLE');
+CREATE TYPE filmart AS ENUM ('ACTION', 'DRAMA', 'ROMANCE');
 
 CREATE TABLE IF NOT EXISTS film (
     id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
     version             integer NOT NULL DEFAULT 0,
     name                varchar(17) NOT NULL UNIQUE USING INDEX TABLESPACE filmspace,
-    sprache             varchar(40),
+    sprache             char(5),
     genre               filmart,
     rating              integer NOT NULL DEFAULT 0,
-    lieferbar           boolean NOT NULL DEFAULT FALSE,
     erscheinungsjahr    date,
     schlagwoerter       varchar(64),
     erzeugt             timestamp NOT NULL DEFAULT NOW(),
